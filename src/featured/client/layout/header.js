aee368e26d801bf3a96c022c42fde8c9d38e643b
Jhans-15 <72289225+JFrancodev12@users.noreply.github.com>
2025-10-15 14:09:35 -0500

    fix:upgrade

--- src/featured/client/layout/header.js
+++ src/featured/client/layout/header.js
@@ -1,10 +1,34 @@
 import { IconShare3, IconShoppingBag } from "@tabler/icons-react";
-import './styles/header.css'
 import { useCart } from "../../context/CartContext";
+import './styles/header.css'
 
 export default function Header () {
 
     const { cart } = useCart();
+
+    const handleShared = async () => {
+        try {
+            const shareData = {
+                title: "Compartir",
+                text: `Mira esta tienda tiene buenos descuentos para ofrecer.`,
+                url: window.location.href,
+            };
+
+            // Guardar en BD que se compartió
+            //await shareRegister({type,id: data?.sub});
+
+            if (navigator.share) {
+                // Si el dispositivo soporta la Web Share API (ej: móviles)
+                await navigator.share(shareData);
+            } else {
+                // En Desktop: redirigir a WhatsApp Web
+                const text = encodeURIComponent(`${shareData.text}: ${shareData.url}`);
+                window.open(`https://wa.me/send?text=${text}`, "_blank");
+            }
+        } catch (err) {
+            console.error("Error al compartir:", err);
+        }
+    }
 
     return (
 
@@ -15,11 +39,11 @@ export default function Header () {
                 <h1>OTY</h1>
 
                 <div className="__left">
-                    <a href='/search' className="__btn">
+                    <a href='/cart' className="__btn">
                         <span className="__bad">{cart.length}</span>
                         <IconShoppingBag/>
                     </a>
-                    <button className="__btn"><IconShare3/></button>
+                    <button className="__btn" onClick={handleShared}><IconShare3/></button>
                 </div>
 
             </div>
